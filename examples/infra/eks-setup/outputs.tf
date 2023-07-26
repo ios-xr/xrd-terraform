157be217de19f3efa33b2378276cc008940f8b47
Tom Johnes <tjohnes@cisco.com>
2023-07-26 10:17:12 +0100

    Remove some eks-setup outputs

--- examples/infra/eks-setup/outputs.tf
+++ examples/infra/eks-setup/outputs.tf
@@ -1,23 +1,3 @@
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = var.cluster_name
-}
-
-output "oidc_issuer" {
-  description = "OIDC issuer URL for the cluster"
-  value       = module.eks_config.oidc_issuer
-}
-
-output "oidc_provider" {
-  description = "OIDC provider ARN for the cluster"
-  value       = module.eks_config.oidc_provider
-}
-
-output "node_iam_instance_profile_name" {
-  description = "Name of the IAM instance profile to be used for worker nodes"
-  value       = module.eks_config.node_iam_instance_profile_name
-}
-
 output "key_name" {
   description = "Key pair name"
   value       = module.key_pair.key_name
