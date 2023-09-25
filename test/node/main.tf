aef88017c7f5ca23cb089f7a2740221967959706
Tom Johnes <tjohnes@cisco.com>
2023-09-25 22:21:54 +0100

    Refactor node MUT
    * Pull setup stuff out of Terraform and into fixtures.
    * Combine everything to a single test module.  The motivation for this
      is that setup has to be function-scoped, which means that class or
      module scoped setup can't work anyway - so combine the defaults test
      cases to a single test_defaults test case.

--- test/node/main.tf
+++ test/node/main.tf
@@ -19,6 +19,18 @@ variable "ami" {
   type = string
 }
 
+variable "iam_instance_profile" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
 variable "instance_type" {
   type    = string
   default = "m5.2xlarge"
@@ -64,84 +76,24 @@ variable "user_data" {
 }
 
 
-data "aws_region" "current" {}
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-
-module "vpc" {
-  source = "../../modules/aws/vpc"
-
-  name = "${var.cluster_name}-vpc"
-  azs  = [data.aws_availability_zones.available.names[0]]
-  cidr = "10.0.0.0/16"
-
-  enable_dns_hostnames = true
-  enable_nat_gateway   = true
-
-  private_subnets         = ["10.0.0.0/24"]
-  public_subnets          = ["10.0.200.0/24"]
-  map_public_ip_on_launch = true
-}
-
-
-module "key_pair" {
-  source = "../../modules/aws/key-pair"
-
-  key_name = "${var.cluster_name}-instance"
-  filename = "${abspath(path.root)}/${var.cluster_name}-instance.pem"
-}
-
-
-data "aws_iam_policy_document" "node" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "node" {
-  assume_role_policy = data.aws_iam_policy_document.node.json
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-  ]
-  name = "${var.cluster_name}-${data.aws_region.current.name}-node"
-}
-
-resource "aws_iam_instance_profile" "node" {
-  name = "${var.cluster_name}-${data.aws_region.current.name}-node"
-  role = aws_iam_role.node.name
-}
-
-
 module "node" {
   source = "../../modules/aws/node"
 
   wait = false
 
-  # Dependent resources that we need to create.
-  iam_instance_profile = aws_iam_instance_profile.node.name
-  key_name             = module.key_pair.key_name
-  subnet_id            = module.vpc.private_subnet_ids[0]
-
-  # Parameters.
-  ami                = var.ami
-  cluster_name       = var.cluster_name
-  kubelet_extra_args = var.kubelet_extra_args
-  instance_type      = var.instance_type
-  name               = var.name
-  network_interfaces = var.network_interfaces
-  private_ip_address = var.private_ip_address
-  security_groups    = var.security_groups
-  user_data          = var.user_data
-  xrd_ami_data       = var.xrd_ami_data
+  ami                  = var.ami
+  cluster_name         = var.cluster_name
+  iam_instance_profile = var.iam_instance_profile
+  instance_type        = var.instance_type
+  key_name             = var.key_name
+  kubelet_extra_args   = var.kubelet_extra_args
+  name                 = var.name
+  network_interfaces   = var.network_interfaces
+  private_ip_address   = var.private_ip_address
+  security_groups      = var.security_groups
+  subnet_id            = var.subnet_id
+  user_data            = var.user_data
+  xrd_ami_data         = var.xrd_ami_data
 }
 
 
--- test/node/test_vars.py
+++ test/node/test_vars.py
@@ -1,14 +1,34 @@
 import base64
+import json
 import logging
+import uuid
 from pathlib import Path
 from typing import Any
 
 import pytest
+from attrs import define
 
-from ..utils import MotoServer, Terraform
-from . import Outputs
+from ..utils import MotoServer, Terraform, TerraformOutputs
 
 logger = logging.getLogger(__name__)
+
+
+@define
+class Outputs(TerraformOutputs):
+    id: str
+    private_ip: str
+
+
+@pytest.fixture(scope="package")
+def this_dir() -> Path:
+    return Path(__file__).parent
+
+
+@pytest.fixture(scope="package")
+def tf(this_dir: Path, moto_server: MotoServer) -> Terraform:
+    tf = Terraform(this_dir, f"http://localhost:{moto_server.port}")
+    tf.init(upgrade=True)
+    return tf
 
 
 @pytest.fixture(autouse=True)
@@ -17,6 +37,104 @@ def reset(moto_server: MotoServer, this_dir: Path) -> None:
     moto_server.reset()
     (this_dir / "terraform.tfstate").unlink(missing_ok=True)
     (this_dir / "test-cluster-instance.pem").unlink(missing_ok=True)
+
+
+@pytest.fixture(autouse=True)
+def vpc(ec2: ...) -> ...:
+    vpcs = list(
+        ec2.vpcs.filter(Filters=[{"Name": "cidr", "Values": ["10.0.0.0/16"]}])
+    )
+    if vpcs:
+        return vpcs[0]
+    return ec2.create_vpc(CidrBlock="10.0.0.0/16")
+
+
+@pytest.fixture(autouse=True)
+def subnet(vpc: ...) -> ...:
+    for subnet in vpc.subnets.all():
+        if subnet.cidr_block == "10.0.0.0/24":
+            return subnet
+    return vpc.create_subnet(
+        AvailabilityZone="eu-west-1a", CidrBlock="10.0.0.0/24"
+    )
+
+
+@pytest.fixture(autouse=True)
+def key_pair(ec2: ...) -> ...:
+    return ec2.create_key_pair(KeyName=str(uuid.uuid4()))
+
+
+@pytest.fixture(autouse=True)
+def iam_instance_profile(iam: ...) -> ...:
+    assume_role_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Effect": "Allow",
+                "Principal": {"Service": "ec2.amazonaws.com"},
+            }
+        ],
+    }
+    role_name = str(uuid.uuid4())
+    role = iam.create_role(
+        RoleName=role_name,
+        AssumeRolePolicyDocument=json.dumps(assume_role_policy),
+    )
+    for policy_arn in (
+        "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+        "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+        "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    ):
+        role.attach_policy(PolicyArn=policy_arn)
+
+    ret = iam.create_instance_profile(InstanceProfileName=str(uuid.uuid4()))
+    ret.add_role(RoleName=role_name)
+    return ret
+
+
+@pytest.fixture
+def base_vars(
+    subnet: ..., key_pair: ..., iam_instance_profile: ...
+) -> dict[str, Any]:
+    return {
+        "ami": "ami-03cf127a",
+        "cluster_name": str(uuid.uuid4()),
+        "name": str(uuid.uuid4()),
+        "network_interfaces": [],
+        "private_ip_address": "10.0.0.10",
+        "subnet_id": subnet.id,
+        "key_name": key_pair.key_name,
+        "iam_instance_profile": iam_instance_profile.name,
+    }
+
+
+def test_defaults(ec2, tf: Terraform, base_vars: dict[str, Any]):
+    tf.apply(vars=base_vars)
+    outputs = Outputs.from_terraform(tf)
+    instance = ec2.Instance(outputs.id)
+
+    assert instance.key_name == base_vars["key_name"]
+    assert instance.private_ip_address == base_vars["private_ip_address"]
+
+    expected_tag_key = f"kubernetes.io/cluster/{base_vars['cluster_name']}"
+    for tag in instance.tags:
+        if tag["Key"] == expected_tag_key:
+            assert tag["Value"] == "owned"
+            break
+    else:
+        assert False, f"tag '{expected_tag_key}' does not exist"
+
+    expected_tag_key = "Name"
+    for tag in instance.tags:
+        if tag["Key"] == expected_tag_key:
+            assert tag["Value"] == base_vars["name"]
+            break
+    else:
+        assert False, f"tag '{expected_tag_key}' does not exist"
+
+    assert not instance.public_ip_address
+    assert not instance.source_dest_check
 
 
 def test_ami(ec2, tf: Terraform, base_vars: dict[str, Any]):
@@ -50,6 +168,14 @@ def test_cluster_name(ec2, tf: Terraform, base_vars: dict[str, Any]):
     assert f"/etc/eks/bootstrap.sh {cluster_name}" in user_data
 
 
+def test_instance_type(ec2, tf: Terraform, base_vars: dict[str, Any]):
+    vars = base_vars | {"instance_type": "m5n.24xlarge"}
+    tf.apply(vars=vars)
+    outputs = Outputs.from_terraform(tf)
+    instance = ec2.Instance(outputs.id)
+    assert instance.instance_type == "m5n.24xlarge"
+
+
 def test_kubelet_extra_args(ec2, tf: Terraform, base_vars: dict[str, Any]):
     vars = base_vars | {"kubelet_extra_args": "cha cha cha"}
     tf.apply(vars=vars)
@@ -64,14 +190,6 @@ def test_kubelet_extra_args(ec2, tf: Terraform, base_vars: dict[str, Any]):
         f"""--kubelet-extra-args '--node-labels=name={base_vars["name"]} cha cha cha'"""
         in user_data
     )
-
-
-def test_instance_type(ec2, tf: Terraform, base_vars: dict[str, Any]):
-    vars = base_vars | {"instance_type": "m5n.24xlarge"}
-    tf.apply(vars=vars)
-    outputs = Outputs.from_terraform(tf)
-    instance = ec2.Instance(outputs.id)
-    assert instance.instance_type == "m5n.24xlarge"
 
 
 def test_name(ec2, tf: Terraform, base_vars: dict[str, Any]):
@@ -94,7 +212,7 @@ def test_private_ip_address(ec2, tf: Terraform, base_vars: dict[str, Any]):
     tf.apply(vars=vars)
     outputs = Outputs.from_terraform(tf)
     instance = ec2.Instance(outputs.id)
-    assert instance.private_ip_address == "10.0.100.10"
+    assert instance.private_ip_address == "10.0.0.200"
 
 
 def test_user_data(ec2, tf: Terraform, base_vars: dict[str, Any]):
