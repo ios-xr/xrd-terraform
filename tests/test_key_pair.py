492183381befaa7bdbfffbc525f65a27b6f62b24
Tom Johnes <tjohnes@cisco.com>
2023-09-27 12:43:35 +0100

    Tidy up key pair

--- tests/test_key_pair.py
+++ tests/test_key_pair.py
@@ -10,15 +10,6 @@ from ._types import MotoServer, Terraform
 
 
 @pytest.fixture(scope="module")
-def base_vars(this_dir: Path) -> dict[str, Any]:
-    key_name = str(uuid.uuid4())
-    return {
-        "key_name": key_name,
-        "filename": str(this_dir / f"{key_name}.pem"),
-    }
-
-
-@pytest.fixture(scope="module")
 def tf(this_dir: Path, moto_server) -> Terraform:
     tf = Terraform(
         this_dir / "terraform" / "key_pair",
@@ -38,9 +29,19 @@ def reset(
     Path(base_vars["filename"]).unlink(missing_ok=True)
 
 
-def test_defaults(ec2: ..., base_vars: dict[str, Any], tf: Terraform):
+@pytest.fixture
+def base_vars(this_dir: Path) -> dict[str, Any]:
+    key_name = str(uuid.uuid4())
+    return {
+        "key_name": key_name,
+        "filename": str(this_dir / f"{key_name}.pem"),
+    }
+
+
+def test_defaults(ec2: ..., tf: Terraform, base_vars: dict[str, Any]):
     tf.apply(vars=base_vars)
 
+    # Assert the key pair exists.
     try:
         ec2.KeyPair(base_vars["key_name"]).load()
     except botocore.exceptions.ClientError as exc:
