009e75146a46c8dd912fab14b231ba38bd9b4157
Tom Johnes <tjohnes@cisco.com>
2023-10-25 22:19:15 +0100

    Reformat

--- tests/test_eks.py
+++ tests/test_eks.py
@@ -120,7 +120,8 @@ def test_defaults(
 
 
 @pytest.mark.parametrize(
-    "cluster_version", ("1.23", "1.24", "1.25", "1.26", "1.27"),
+    "cluster_version",
+    ("1.23", "1.24", "1.25", "1.26", "1.27"),
 )
 def test_cluster_version(
     eks_client: EKSClient,
