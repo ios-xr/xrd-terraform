a40795897dbd318e6e41b034372a265abffaa41b
Tom Johnes <tjohnes@cisco.com>
2023-11-03 17:41:44 +0000

    Improve Bootstrap variables description

--- modules/aws/bootstrap/variables.tf
+++ modules/aws/bootstrap/variables.tf
@@ -15,7 +15,10 @@ variable "cluster_version" {
 }
 
 variable "name" {
-  description = "Used as a prefix for the 'Name' tag for each created resource"
+  description = <<-EOT
+  Used as a prefix for the 'Name' tag for each created resource".
+  If null, then a random name 'xrd-terraform-[0-9a-z]{8}' is used.
+  EOF
   type     = string
-  nullable = false
+  default = null
 }
