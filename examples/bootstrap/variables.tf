0f24ac28660b56b321235f0fb1962e43ac4feb25
Tom Johnes <tjohnes@cisco.com>
2023-11-22 15:39:10 +0000

    Fixup variable description

--- examples/bootstrap/variables.tf
+++ examples/bootstrap/variables.tf
@@ -1,8 +1,8 @@
 variable "name" {
   description = <<-EOT
-  Used as a prefix for the 'Name' tag for each created resource".
+  Used as a prefix for the 'Name' tag for each created resource.
   If null, then a random name 'xrd-terraform-[0-9a-z]{8}' is used.
-  EOF
+  EOT
   type     = string
   default = null
 }
--- modules/aws/bootstrap/variables.tf
+++ modules/aws/bootstrap/variables.tf
@@ -16,9 +16,9 @@ variable "cluster_version" {
 
 variable "name" {
   description = <<-EOT
-  Used as a prefix for the 'Name' tag for each created resource".
+  Used as a prefix for the 'Name' tag for each created resource.
   If null, then a random name 'xrd-terraform-[0-9a-z]{8}' is used.
-  EOF
+  EOT
   type     = string
   default = null
 }
