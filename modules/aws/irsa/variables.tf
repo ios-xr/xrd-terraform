29a6c955738748652652a27fd3e8f75828c94c16
Tom Johnes <tjohnes@cisco.com>
2023-11-28 14:40:07 +0000

    Pre-commit

--- modules/aws/irsa/variables.tf
+++ modules/aws/irsa/variables.tf
@@ -11,29 +11,29 @@ variable "role_policies" {
   description = "List of role policy ARNs to attach to the role"
   type        = list(string)
   default     = []
-  nullable = false
+  nullable    = false
 }
 
 variable "oidc_issuer" {
   description = "OIDC issuer URL of the EKS cluster to create an IRSA IAM role for"
   type        = string
-  nullable = false
+  nullable    = false
 }
 
 variable "oidc_provider" {
   description = "OIDC provider ARN of the EKS cluster to create an IRSA IAM role for"
   type        = string
-  nullable = false
+  nullable    = false
 }
 
 variable "namespace" {
   description = "Namespace of the service account to create the role for. Use '*' for a wildcard"
   type        = string
-  nullable = false
+  nullable    = false
 }
 
 variable "service_account" {
   description = "Name of the service account to create the role for. Use '*' for a wildcard"
   type        = string
-  nullable = false
+  nullable    = false
 }
--- modules/aws/linux-pod-with-net-attach/variables.tf
+++ modules/aws/linux-pod-with-net-attach/variables.tf
@@ -1,35 +1,35 @@
 variable "name" {
   description = "Name of the Pod"
-  type = string
-  nullable = false
+  type        = string
+  nullable    = false
 }
 
 variable "device" {
   description = "Name of the device to move from the host network namespace to the container network namespace"
-  type = string
-  nullable = false
+  type        = string
+  nullable    = false
 }
 
 variable "ip_address" {
   description = "IP address to assign to the device"
-  type = string
-  nullable = false
+  type        = string
+  nullable    = false
 }
 
 variable "gateway" {
   description = "Gateway address"
-  type = string
-  nullable = false
+  type        = string
+  nullable    = false
 }
 
 variable "routes" {
   description = "List of routes to add to the container network namespace"
-  type = list(string)
-  nullable = false
+  type        = list(string)
+  nullable    = false
 }
 
 variable "node_selector" {
   description = "Map of labels to values to use as the Pod node selector"
-  type    = map(string)
-  default = null
+  type        = map(string)
+  default     = null
 }
