703a3df1ae8ce80750421b4c0bd6d02e30c003cf
Tom Johnes <tjohnes@cisco.com>
2024-02-06 11:41:52 +0000

    Add variable to choose two AZs in which to launch the cluster

--- examples/bootstrap/main.tf
+++ examples/bootstrap/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 module "bootstrap" {
   source = "../../modules/aws/bootstrap"
 
-  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  azs             = coalesce(var.azs, slice(data.aws_availability_zones.available.names, 0, 2))
   cluster_version = var.cluster_version
   name_prefix     = var.name_prefix
 }
