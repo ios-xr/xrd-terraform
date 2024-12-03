d2fbbc9485e77126985e6a80584aafd7f570ead4
Ed Beaty <ebeaty@cisco.com>
2024-12-03 04:48:12 -0800

    .

--- modules/aws/node/main.tf
+++ modules/aws/node/main.tf
@@ -159,7 +159,7 @@ resource "aws_instance" "this" {
       name = data.aws_eks_cluster.this.name
       api_endpoint = data.aws_eks_cluster.this.endpoint
       certificate_authority = data.aws_eks_cluster.this.certificate_authority[0].data
-      cidr = data.aws_eks_cluster.this.vpc_config[0].public_access_cidr
+      cidr = data.aws_eks_cluster.this.vpc_config[0].public_access_cidrs[0]
     }
   )
 
