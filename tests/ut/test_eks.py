7083e9ec78f1d538e547247676210250acb60f8a
ebeaty-cisco <150805304+ebeaty-cisco@users.noreply.github.com>
2025-06-12 13:56:39 +0100

    Remove EKS 1.25 (#42)

--- tests/ut/test_eks.py
+++ tests/ut/test_eks.py
@@ -124,12 +124,12 @@ def test_defaults(
 @pytest.mark.parametrize(
     "cluster_version",
     (
-        "1.25",
         "1.26",
         "1.27",
         "1.28",
         "1.29",
         "1.30",
+        "1.31",
     ),
 )
 def test_cluster_version(
